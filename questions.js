949099a0dd35a600999ef4f4686d8b5ed54297f0
s036658 <s036658@students.lmsd.org>
2025-05-08 08:55:56 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,13 @@
+export const questions = [
+  {
+    type: "multiple_choice",
+    question: "What is the powerhouse of the cell?",
+    options: ["Nucleus", "Mitochondria", "Ribosome"],
+    answer: "Mitochondria"
+  },
+  {
+    type: "true_false",
+    question: "DNA stands for Deoxyribonucleic Acid.",
+    answer: true
+  }
+];
